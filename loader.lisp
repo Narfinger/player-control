9711a5075bad5b83cada0318dcec64ca2aa9ccde
Christian Engels <s9chenge@stud.uni-saarland.de>
2011-05-01 18:34:31 +0200

    changed loader.lisp for new hunchentoot version and newer asdf version

--- loader.lisp
+++ loader.lisp
@@ -1,11 +1,13 @@
 (require :asdf)
 (pushnew "amarok-control" asdf:*central-registry* :test #'equal)
 
-(asdf:load-system :amarok-control)
+;(asdf:load-system :amarok-control)
+(require :amarok-control)
 
 (in-package :amarok-control)
 
 (defconstant server-port 8080)
 (defconstant server-address "192.168.24.1")
 
-(defvar *ht-server* (hunchentoot:start-server :port server-port :address server-address))
+(hunchentoot:start (make-instance 'hunchentoot:acceptor :port server-port))
+;(defvar *ht-server* (hunchentoot:start-server :port server-port :address server-address))
