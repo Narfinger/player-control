91affd485ea83628db581f6e483c2f394fac5ec9
Christian Engels(Laptop) <s9chenge@stud.uni-saarland.de>
2014-01-16 22:44:51 +0100

    some more changes

--- amarok.go
+++ amarok.go
@@ -20,18 +20,29 @@ type StatusPage struct {
 
 func getStatus(object *dbus.Object ) *StatusPage {
 	var songinfo map[string]dbus.Variant
-	fmt.Println("start print")
-	reply := object.Call("org.freedesktop.MediaPlayer.GetMetadata", 0).Store(&songinfo)
+	var statusinfo [1][4]int 
+	object.Call("org.freedesktop.MediaPlayer.GetMetadata", 0).Store(&songinfo)
+
+	//does not work -.-
+	reply := object.Call("org.freedesktop.MediaPlayer.GetStatus", 0).Store(&statusinfo)
 	fmt.Println(reply)
+	fmt.Println(statusinfo)
 
-	title := songinfo["title"].String()
-	artist := songinfo["artist"].String()
-	album := songinfo["album"].String()
-
-	data := StatusPage{Title: title, Artist: artist, Album: album, StatusM: "tmpstatus", StatusS: "seriestatus"}
-	fmt.Println(data)
-	fmt.Println("end print")
-	return &data
+	data := StatusPage{Title: "", Artist: "", Album: "", StatusM: "", StatusS: ""}
+	if songinfo != nil {
+		title := songinfo["title"].String()
+		artist := songinfo["artist"].String()
+		album := songinfo["album"].String()
+		
+		data.Title = title
+		data.Artist = artist
+		data.Album = album
+		data.StatusM = "tmpstatus"
+		data.StatusS = "seriestatus"
+		return &data
+	} else {
+		return &data
+	}
 }
 
 
@@ -41,32 +52,36 @@ func executeHandler(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request, object *dbus.Object) {
 	if r.URL.Path != "/" {
 		http.NotFound(w,r)
 		return
 	}
 	t, _ := template.ParseFiles("amarok.html")
-	
-	data := StatusPage{Title: "tmptitle", Artist: "tmpartist", Album: "tmpalbum", StatusM: "tmpstatus", StatusS: "seriestatus"}
+	data := getStatus(object)
+
 	t.Execute(w,data)
 
 }
 
 // dbus has signals, i should connect and do stuff with it instead of query everytime!!!
-
 func main() {
-	fmt.Println("Starting Server on port")
-	http.HandleFunc("/execute", executeHandler)
-	http.HandleFunc("/", indexHandler)
-
 	conn, error := dbus.SessionBus()
 	if error != nil {
 		panic(error)
 	}
 
 	object := conn.Object("org.mpris.clementine", "/Player")
+	
 	getStatus(object)
+	
+	fmt.Println("Starting Server on port")
+	http.HandleFunc("/execute", executeHandler)
+	http.HandleFunc("/style.css", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w,r, "style.css")})
+	http.HandleFunc("/", func( w http.ResponseWriter, r *http.Request) {
+		indexHandler(w,r,object)})
 
 	http.ListenAndServe(":8082", nil)
+	fmt.Println("Stopping")
 }
