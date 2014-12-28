f73c16a4983a2a9fd862f302909f7b4a753000db
Narfinger <Narfinger@users.noreply.github.com>
2014-12-28 22:42:06 +0100

    minor changes

--- haskell/DBusController.hs
+++ haskell/DBusController.hs
@@ -6,26 +6,21 @@ module DBusController ( SongInfo(..)
                       ) where
 
 import Data.Int
-import qualified Data.Map as M 
+import qualified Data.Map as M
 import Data.Maybe
 import Data.String
 import DBus
 import DBus.Client
-import Control.Monad (forever,liftM)
-import Control.Monad.Trans  (lift, liftIO)
-import Control.Concurrent (threadDelay)
 import Debug.Trace (trace)
 
-
-
-data PlayStatus = Playing | Stopped | Paused  deriving (Show)
+data MusicStatus = Playing | Stopped | Paused  deriving (Show)
 data SerieviewerStatus = Running | NotRunning deriving (Show)
 
 data SongInfo = SongInfo { title :: String
                          , artist :: String
                          , album :: String
                          } deriving (Show)
-data StatusInfo = StatusInfo { statusmusic :: Maybe PlayStatus
+data StatusInfo = StatusInfo { statusmusic :: Maybe MusicStatus
                              , statusserie :: Maybe SerieviewerStatus
                              } deriving (Show)
 
@@ -34,7 +29,7 @@ callMedia client path method =
    call_ client (methodCall (objectPath_ path) "org.freedesktop.MediaPlayer" (memberName_ method))
         { methodCallDestination = Just "org.mpris.clementine"
         }
-  
+
 callPlayer :: Client -> String -> IO MethodReturn
 callPlayer client method = callMedia client "/Player" method
 
@@ -50,36 +45,44 @@ getTrackInfo client id =
     methodCallBody = [toVariant id]
   }
 
-createPlayStatus :: Int -> PlayStatus
-createPlayStatus 0 = Playing
-createPlayStatus 1 = Paused
-createPlayStatus 2 = Stopped
+createMusicStatus :: (Num a, Eq a) => a -> Maybe MusicStatus
+createMusicStatus 0 = Just Playing
+createMusicStatus 1 = Just Paused
+createMusicStatus 2 = Just Stopped
+createMusicStatus _ = Nothing
 
 fromMaybeVariant :: (IsVariant a) => a -> Variant -> a
 fromMaybeVariant def v =
-    let defv = toVariant def in 
+    let defv = toVariant def in
     fromMaybe def $ fromVariant v
-                     
+
 lookupDictionary :: (IsVariant a) => String -> a -> [(Variant, Variant)] -> a
 lookupDictionary key def dict =
     let k = toVariant key
         defv = toVariant ("-" ::String)
         v = fromMaybe defv $ lookup k dict in
     fromMaybeVariant def $ fromMaybeVariant defv v
-                         
-extractTrackID :: MethodReturn -> Maybe Int32
-extractTrackID method = fromVariant $ head $ methodReturnBody method
 
 extractTrackInfo :: MethodReturn -> SongInfo
 extractTrackInfo method =
-  let v =  head $ methodReturnBody method  in 
+  let v =  head $ methodReturnBody method  in
   let Just body =  fromVariant v :: Maybe Dictionary
-      dict = dictionaryItems body 
-      title'  = lookupDictionary "title"  "-" dict 
+      dict = dictionaryItems body
+      title'  = lookupDictionary "title"  "-" dict
       artist' = lookupDictionary "artist" "-" dict
       album'  = lookupDictionary "album"  "-" dict in
   SongInfo { title = title', artist = artist', album = album' }
-  
+
+extractTrackID :: MethodReturn -> Maybe Int32
+extractTrackID method = fromVariant $ head $ methodReturnBody method
+
+extractPlayStatusInfo :: MethodReturn -> Maybe MusicStatus
+extractPlayStatusInfo method =
+    let v = head $ methodReturnBody method in
+    let body = (fromVariant v) :: Maybe (Int32, Int32, Int32, Int32) in
+    --trace $ variantType v
+    createMusicStatus $ fromMaybeVariant (0::Int32) $ fst body
+
 getSongInfo :: Client -> IO SongInfo
 getSongInfo client = do
   let methodreturn = callTrack client "GetCurrentTrack"
@@ -87,10 +90,8 @@ getSongInfo client = do
   tinfo <- getTrackInfo client id;
   return (extractTrackInfo tinfo)
 
--- getMusicStatusInfo :: Client -> IO PlayStatus
--- getMusicStatusInfo client = do
-  
-
 getStatusInfo :: Client -> IO StatusInfo
 getStatusInfo client = do
-  return StatusInfo { statusserie = Nothing, statusmusic = Nothing }
+  let methodreturn = callPlayer client "GetStatus"
+  statusm' <- fmap extractPlayStatusInfo methodreturn;
+  return StatusInfo { statusserie = Nothing, statusmusic = statusm'}
